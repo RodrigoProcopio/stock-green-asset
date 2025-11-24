b7e2fc440c7ab000b841d9952a18615ee6edcf35
RodrigoProcopio <digoprocopio@gmail.com>
2025-11-24 16:52:41 -0300

    Ajuste de advisory member

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import { useTranslation } from "react-i18next";
-import logoStock from "../../assets/logo-stock.png";
+import logoStock from "../../assets/logo-stock.webp";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -128,7 +128,7 @@ export function Navbar() {
               <motion.img
                 src={logoStock}
                 alt="Stock Capital"
-                className="absolute left-0 h-50 w-54"
+                className="absolute left-0 h-10 w-auto"
                 initial={{ y: "0%" }}
                 animate={logoTopControls}
                 style={{ willChange: "transform" }}
@@ -137,7 +137,7 @@ export function Navbar() {
               <motion.img
                 src={logoStock}
                 alt="Stock Capital"
-                className="absolute left-0 h-50 w-54"
+                className="absolute left-0 h-10 w-auto"
                 initial={{ y: "-100%" }}
                 animate={logoBottomControls}
                 style={{ willChange: "transform" }}
